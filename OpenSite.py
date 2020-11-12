a860d756d15cf717057458d516d6ec3ae9809d3d
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-11-12 18:46:21 -0300

    agrego coding=utf8

--- OpenSite.py
+++ OpenSite.py
@@ -1,8 +1,9 @@
+# coding=utf-8
 
 import webbrowser
 
 try:
-    # abr en navegador Chromium, esta abrir la página de Google
+    # abrir en navegador Chromium, una nueva pestaña con la url indicada
     # si llamamos al metodo open_new, se abrirá una nueva ventana de chromium
     webbrowser.get("chromium").open("https://www.google.com/")
     pass
