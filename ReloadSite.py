a7501d21010f7547a967ba57999e2adeb20f7eac
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-11-12 20:31:27 -0300

    obtengo info de ping (con bash) desde python

--- ReloadSite.py
+++ ReloadSite.py
@@ -4,8 +4,18 @@ import subprocess
 import webbrowser
 
 try:
+    # llamo a un subproceso para ejecutar el script:
     subprocess.call("./CheckInternetCon.sh", shell=True)
-    
+
+    # obtengo el porcentaje de paquetes perdidos a hacer un ping (con Script CheckInternetCon.sh)
+    file = open("pingData.txt", "r")
+    porcentaje = int(file.read(1))    
+    file.close()
+
+    if porcentaje >= 50:
+        print("Mala conexion")
+    else:
+        print("Buena conexion.")
 
 except:
     print("No se pudo llamar al subproceso.")
